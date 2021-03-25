f4b73db3c98f158dc43471bdb63c54a442bfb908
Tim Joosten <Tjoosten@users.noreply.github.com>
2021-03-25 11:42:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -57,52 +57,6 @@ Route::group(['prefix' => 'account', 'middleware' => ['auth'], 'as' => 'account.
 	Route::post('twofactor/verify', 'Account\TwoFactorController@verify')->name('twofactor.verify');
 	Route::delete('twofactor', 'Account\TwoFactorController@destroy')->name('twofactor.destroy');
 
-	/**
-	 * subscription
-	 */
-	Route::group(['prefix' => 'subscription', 'namespace' => 'Account\Subscription', 'as' => 'subscription.', 'middleware' => ['subscription.owner']], function() {
-		/**
-		 * card
-		 */
-		Route::group(['middleware' => 'subscription.customer'], function() {
-			Route::get('/card', 'SubscriptionCardController@index')->name('card.index');
-			Route::post('/card', 'SubscriptionCardController@store')->name('card.store');
-		});
-		/**
-		 * resume
-		 */
-		Route::group(['middleware' => 'subscription.cancelled'], function() {
-			Route::get('/resume', 'SubscriptionResumeController@index')->name('resume.index');
-			Route::post('/resume', 'SubscriptionResumeController@store')->name('resume.store');
-		});
-		/**
-		 * cancel
-		 */
-		Route::group(['middleware' => 'subscription.notcancelled'], function() {
-			Route::get('/cancel', 'SubscriptionCancelController@index')->name('cancel.index');
-			Route::post('/cancel', 'SubscriptionCancelController@store')->name('cancel.store');
-		});
-		/**
-		 * swap
-		 */
-		Route::group(['middleware' => 'subscription.notcancelled'], function() {
-			Route::get('/swap', 'SubscriptionSwapController@index')->name('swap.index');
-			Route::post('/swap', 'SubscriptionSwapController@store')->name('swap.store');
-		});
-
-		/**
-		 * team
-		 */
-		Route::group(['middleware' => 'subscription.team'], function() {
-			Route::get('/team', 'SubscriptionTeamController@index')->name('team.index');
-			Route::patch('/team', 'SubscriptionTeamController@update')->name('team.update');
-
-			Route::post('/team/member', 'SubscriptionTeamMemeberController@store')->name('team.member.store');
-			Route::delete('/team/member/{user}', 'SubscriptionTeamMemeberController@destroy')->name('team.member.destroy');
-		});
-	});
-});
-
 /**
  * Account Activation
  */
@@ -122,14 +76,6 @@ Route::group(['prefix' => 'plans', 'as' => 'plans.', 'middleware' => ['subscript
     Route::get('/teams', 'Subscription\PlanTeamController@index')->name('teams.index');
 });
 
-/**
- * Subscription Activation
- */
-
-Route::group(['prefix' => 'subscription', 'as' => 'subscription.', 'middleware' => ['auth.register', 'subscription.inactive']], function() {
-    Route::get('/', 'Subscription\SubscriptionController@index')->name('index');
-    Route::post('/', 'Subscription\SubscriptionController@store')->name('store');
-});
 
 /**
  * Webhooks
